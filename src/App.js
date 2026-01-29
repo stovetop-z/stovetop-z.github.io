eb6689051762d85fdee03058c14cb1af28a80ea0
stovetop-z <steven.zinnbr@gmail.com>
2026-01-29 11:20:14 -0600

    First website update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState} from 'react';
+
 function App() {
+
+  const [view, setView] = useState("Home");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="banner">
+        <Button name="btn" sym="⌂" onClick={() => setView("Home")} />
+        <Button name="btn" sym="Projects" onClick={() => setView("Projects")} />
+        <Button name="btn" sym="About" onClick={() => setView("About")} />
+      </div>
+      <div className="std-view"> 
+        {view == "Home" && <Home />}
+        {view == "Projects" && <Projects />}
+        {view == "About" && <About />}
+      </div>
+    </>
   );
+}
+
+function Home() {
+  return <h3>Steven Zinn</h3>;
+}
+
+function Projects() {
+  return <h3>My Projects</h3>;
+}
+
+function About() {
+  return <h3>About Steven Zinn</h3>;
+}
+
+function Button({name, sym, onClick}) {
+  return <button className={name} onClick={onClick}>{sym}</button>;
 }
 
 export default App;
